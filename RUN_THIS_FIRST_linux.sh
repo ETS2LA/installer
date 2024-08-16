4e092babc4fbc3c611ef7128fdcbb42f94bc2a24
Cloud hayes <72272909+Cloud-121@users.noreply.github.com>
2024-08-16 14:26:17 -0500

    Update RUN_THIS_FIRST_linux.sh

--- RUN_THIS_FIRST_linux.sh
+++ RUN_THIS_FIRST_linux.sh
@@ -6,9 +6,7 @@ if [ $? -ne 0 ]; then
     if [ $? -ne 0 ]; then
         echo "- Error: Incompatible Python version found"
         echo ""
-        echo "Please install either Python 3.10 or 3.11 and make sure it is set as the default Python installation."
-        echo "https://www.python.org/downloads/release/python-3119/  -  Scroll down and download \"Windows installer (64-bit)\""
-        echo "Make sure to toggle \"Add Python to PATH\" during installation."
+        echo "Please install either Python 3.10 or 3.11."
         echo ""
         read -n1 -r -p "Press any key to continue..."
         exit
@@ -28,7 +26,7 @@ if [ $? -ne 0 ]; then
 	echo "- Error: NPM not installed"
 	echo "? Continuing will try and install Node automatically, CTRL+C to cancel"
 	read -n1 -r -p "Press any key to continue..."
-	python helpers/download_node.py
+	python helpers/download_node_linux.py
 	echo ""
 	echo "Please restart the script to check if Node was installed correctly."
 	echo "If it's not working then you can install it manually from https://nodejs.org/en"
@@ -41,7 +39,7 @@ if [ $? -ne 0 ]; then
 	echo "- Error: Git not installed"
 	echo "? Continuing will try and install Git automatically, CTRL+C to cancel"
 	read -n1 -r -p "Press any key to continue..."
-	python helpers/download_git.py
+	python helpers/download_git_linux.py
 	echo ""
 	echo "Please restart the script to check if Git was installed correctly."
 	echo "If it's not working then you can install it manually from https://git-scm.com/downloads"
